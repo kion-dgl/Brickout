abba5dd9156fd53617b0791b55a031c32e963e81
Benjamin Collins <admin@termantics.com>
2017-01-08 11:04:49 +0900

    drawing a paddle

---
+++ 05_draw_a_paddle/main.c
@@ -0,0 +1,123 @@
+#include <math.h>
+#include <GL/freeglut.h>
+
+#define WIDTH  640
+#define HEIGHT 480
+
+struct {
+	GLfloat x;
+	GLfloat y;
+	GLfloat radius;
+	GLfloat dy;
+	GLfloat dx;
+} ball;
+
+struct {
+	GLfloat x;
+	GLfloat y;
+	GLfloat dx;
+	GLfloat width;
+	GLfloat height;
+} paddle;
+
+void init();
+void display();
+void timer(int value);
+
+int main(int argc, char *argv[]) {
+
+	glutInit(&argc, argv);
+	glutInitWindowSize(WIDTH, HEIGHT);
+	glutCreateWindow("OpenGL - Brickout");
+	glutDisplayFunc(display);
+	glutTimerFunc(0, timer, 0);
+	init();
+	glutMainLoop();
+
+}
+
+void init() {
+
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+	glViewport(0, 0, WIDTH, HEIGHT);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(0, WIDTH, 0, HEIGHT);
+
+	ball.x = 320.0f;
+	ball.y = 240.0f;
+	ball.radius = 14.0f;
+	ball.dx = 3.0f;
+	ball.dy = -3.0f;
+
+	paddle.x = 320.0f;
+	paddle.y = 20.0f;
+	paddle.height = 18.0f;
+	paddle.width = 60.0f;
+	paddle.dx = 4.0f;
+
+}
+
+void display() {
+
+	glClear(GL_COLOR_BUFFER_BIT);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	glPushMatrix();
+
+	glTranslatef(ball.x, ball.y, 0.0f);
+	glBegin(GL_TRIANGLE_FAN);
+	glColor3f(1.0f, 1.0f, 1.0f);
+	glVertex2f(0.0f, 0.0f);
+
+	GLfloat angle;
+	int i;
+
+	for(i = 0; i <= 100; i++) {
+		angle = i * 2.0f * M_PI / 100;
+		glVertex2f(cos(angle) * ball.radius, sin(angle) * ball.radius);
+	}
+	
+	glEnd();
+	glPopMatrix();
+
+	glPushMatrix();
+	glTranslatef(paddle.x, paddle.y, 0.0f);
+	glBegin(GL_QUADS);
+	glColor3f(1.0f, 1.0f, 1.0f);
+	glVertex2f(-paddle.width, 0.0f);
+	glVertex2f( paddle.width, 0.0f);
+	glVertex2f( paddle.width, paddle.height);
+	glVertex2f(-paddle.width, paddle.height);
+	glEnd();
+	glPopMatrix();
+
+	glutSwapBuffers();
+
+	ball.x += ball.dx;
+	ball.y += ball.dy;
+
+	if(ball.x < ball.radius) {
+		ball.x = ball.radius;
+		ball.dx = -ball.dx;
+	} else if(ball.x > WIDTH - ball.radius) {
+		ball.x = WIDTH - ball.radius;
+		ball.dx = -ball.dx;
+	}
+
+	if(ball.y < ball.radius) {
+		ball.y = ball.radius;
+		ball.dy = -ball.dy;
+	} else if(ball.y > HEIGHT - ball.radius) {
+		ball.y = HEIGHT - ball.radius;
+		ball.dy = -ball.dy;
+	}
+
+}
+
+void timer(int value) {
+	
+	glutPostRedisplay();
+	glutTimerFunc(30, timer, 0);
+
+}
---
+++ 05_draw_a_paddle/readme.md
@@ -0,0 +1,3 @@
+#Move a ball
+
+![alt tag](https://github.com/termantics/OpenGL-Brickout/blob/master/05_draw_a_paddle/output.png?raw=true)
